6f90feffeeaf9835629141c52f146a7dbde799bc
蛋頭 <vsn80395@gmail.com>
2025-09-30 21:06:16 +0800

    iuyjythrg3

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
 import mysql.connector
+from flask import Flask
 
-def query_mysql():
-    # 建立連線
+app = Flask(__name__)
+
+@app.route('/products')
+def get_products():
     conn = mysql.connector.connect(
         host='localhost',
         user='testuser',
         password='testpass',
-        database='testdb'
+        database='testdb',
+        port=3306
     )
     cursor = conn.cursor()
-
-    # 執行查詢
     cursor.execute('SELECT * FROM Product')
     results = cursor.fetchall()
-
-    # 輸出結果
-    for row in results:
-        print(row)
-
-    # 關閉連線
+    columns = [desc[0] for desc in cursor.description]
     cursor.close()
     conn.close()
+    # 直接以純文字格式印出
+    output = '\t'.join(columns) + '\n'
+    for row in results:
+        output += '\t'.join(str(item) for item in row) + '\n'
+    return f'<pre>{output}</pre>'
+
+@app.route('/good')
+def good():
+    return 'This is the /good route. Service is running!'
 
 if __name__ == '__main__':
-    query_mysql()
+    app.run(host='0.0.0.0', port=8080)
